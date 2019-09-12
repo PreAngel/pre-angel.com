0680659d2da63ab87f6af6ef1fa541568dbed122
Huan LI <zixia@zixia.net>
2019-09-12 20:31:05 +0800

    fit image for ev

--- scripts/fit-image.sh
+++ scripts/fit-image.sh
@@ -3,7 +3,6 @@ set -e
 
 function resize () {
   FILE=$1
-  echo "resizing $FILE ..."
   mogrify \
     -verbose \
     -quality 80 \
@@ -25,18 +24,26 @@ pushd ${DST}
 # FILES=$(git ls-files --exclude-standard --others *.jpg)
 FILE_LIST=($(find . -type f -name '*.jpg' -o -name '*.jpeg' -o -name '*.png' -o -name '*.gif'))
 
+echo "Checking ..."
 for FILE in "${FILE_LIST[@]}"; do
   if [[ $FILE =~ assets/peoples/preangel/ ]]; then
-    echo "skipped $FILE for it's in whitelist directory."
+    # echo "skipped $FILE for it's in whitelist directory."
+    echo -n S
     continue
   fi
 
-  echo "checking $FILE ..."
   WIDTH=$(identify -ping -format '%w' "$FILE")
   if [ $WIDTH -gt 1920 ]; then
     resize $FILE
+    echo "Resized $FILE"
+  else
+    echo -n O
   fi
+
 done
+
+echo
+echo Done.
 
 popd
 
